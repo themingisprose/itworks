acdefb33b66ca4e7188b5b4dee5079ff1f22ec9f
rogertm <roger213tm@gmail.com>
2023-07-13 17:23:27 -0400

    Ok! It works

---
+++ itworks-template.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<?php wp_head(); ?>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet">
+	<style>
+		body{
+			background: #fff;
+			font-family: 'Ubuntu', sans-serif;
+		}
+		main{
+			height: 100vh;
+			width: 100vw;
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+		}
+	</style>
+</head>
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+
+	<main>
+		<strong><?php _e( 'It works', 'itworks' ) ?></strong>
+		<div>¯\_(ツ)_/¯</div>
+	</main>
+
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ itworks.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Plugin Name: It works
+ * Description: ¯\_(ツ)_/¯
+ * Plugin URI: https://themingisprose.com
+ * Author: Theming is Prse
+ * Author URI: https://themingisprose.com
+ * Version: 1.0.0
+ * License: GPL2
+ * Text Domain: itworks
+ * Domain Path: /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) )
+	die;
+
+/**
+ * Set text domain... If any
+ *
+ * @since It Works 1.0.0
+ */
+function itworks_textdomain(){
+	load_plugin_textdomain( 'itworks', false, basename( dirname( __FILE__ ) ) . '/languages/' );
+}
+add_action( 'plugins_loaded', 'itworks_textdomain' );
+
+/**
+ * Load the template
+ *
+ * @since it Works 1.0.0
+ */
+function itworks_load_template( $template ){
+	/**
+	 * Filters the loaded template
+	 *
+	 * @since It Works 1.0.0
+	 */
+	return apply_filters( 'itworks_template', plugin_dir_path( __FILE__ ) .'itworks-template.php' );
+}
+add_filter( 'index_template', 'itworks_load_template' );
+add_filter( '404_template', 'itworks_load_template' );
+add_filter( 'archive_template', 'itworks_load_template' );
+add_filter( 'author_template', 'itworks_load_template' );
+add_filter( 'category_template', 'itworks_load_template' );
+add_filter( 'tag_template', 'itworks_load_template' );
+add_filter( 'taxonomy_template', 'itworks_load_template' );
+add_filter( 'date_template', 'itworks_load_template' );
+add_filter( 'home_template', 'itworks_load_template' );
+add_filter( 'front_page_template', 'itworks_load_template' ); /** $#@*8! */
+add_filter( 'frontpage_template', 'itworks_load_template' );
+add_filter( 'page_template', 'itworks_load_template' );
+add_filter( 'paged_template', 'itworks_load_template' );
+add_filter( 'search_template', 'itworks_load_template' );
+add_filter( 'single_template', 'itworks_load_template' );
+add_filter( 'text_template', 'itworks_load_template' );
+add_filter( 'plain_template', 'itworks_load_template' );
+add_filter( 'text_plain_template', 'itworks_load_template' );
+add_filter( 'attachment_template', 'itworks_load_template' );
+add_filter( 'comments_popup', 'itworks_load_template' );
